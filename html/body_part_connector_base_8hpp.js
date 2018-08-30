770d8a7450835becbcfb467719694fe2107c11ee
Travis CI <travis@travis-ci.org>
2018-08-30 14:41:37 +0000

    Deploy code docs to GitHub Pages Travis build: 350
    Commit: 4b6e4b07f4e38a0bf431f5822150a4b3498dd728

--- html/body_part_connector_base_8hpp.js
+++ html/body_part_connector_base_8hpp.js
@@ -3,6 +3,6 @@ var body_part_connector_base_8hpp =
     [ "connectBodyPartsCpu", "body_part_connector_base_8hpp.html#a708e05033d5c51c3a3ff5d4ed621ebbf", null ],
     [ "connectBodyPartsGpu", "body_part_connector_base_8hpp.html#a047afd1856d394bec9b19dab7e765029", null ],
     [ "generateInitialSubsets", "body_part_connector_base_8hpp.html#a010b3f1a08cfb1d22ac57c2b8b6f52db", null ],
-    [ "removeSubsetsBelowThresholds", "body_part_connector_base_8hpp.html#a6c91d298fdc65dc2cfa967e47ceda1d3", null ],
+    [ "removeSubsetsBelowThresholds", "body_part_connector_base_8hpp.html#afa14183f5226b602154559fc94d49dff", null ],
     [ "subsetsToPoseKeypointsAndScores", "body_part_connector_base_8hpp.html#a7840872b74edcb234b70e396fa35d7d6", null ]
 ];
--- html/render_pose_8hpp.js
+++ html/render_pose_8hpp.js
@@ -1,10 +1,10 @@
 var render_pose_8hpp =
 [
-    [ "renderPoseDistance", "render_pose_8hpp.html#aaaa8fe7bb8ed4d5aada483e45cdec576", null ],
-    [ "renderPoseHeatMapGpu", "render_pose_8hpp.html#a9a3b197334a6a606f85eea43128a9a46", null ],
-    [ "renderPoseHeatMapsGpu", "render_pose_8hpp.html#aa01e90a66388f8ced55d0aa61fb205a1", null ],
+    [ "renderPoseDistance", "render_pose_8hpp.html#ae45299199877cae41fb2c71ab65d4508", null ],
+    [ "renderPoseHeatMapGpu", "render_pose_8hpp.html#a1f851b71bd5136ae78a1b63ddb228ed4", null ],
+    [ "renderPoseHeatMapsGpu", "render_pose_8hpp.html#a5afd7c8ba1a5db63538d1f0182fb83b4", null ],
     [ "renderPoseKeypointsCpu", "render_pose_8hpp.html#a99a08148f440bd96546076e15f0de04c", null ],
     [ "renderPoseKeypointsGpu", "render_pose_8hpp.html#ab79ad497b3b69db2d233656a2e97bc63", null ],
-    [ "renderPosePAFGpu", "render_pose_8hpp.html#a832678e25561cd77d174847b0dddc6bb", null ],
-    [ "renderPosePAFsGpu", "render_pose_8hpp.html#a7e2c7f463bbbbe9665faa46c073cc5ac", null ]
+    [ "renderPosePAFGpu", "render_pose_8hpp.html#adfce844d710e62e639e0c0610da1f809", null ],
+    [ "renderPosePAFsGpu", "render_pose_8hpp.html#a09b876085ca58aea2553b1b8039854c8", null ]
 ];
