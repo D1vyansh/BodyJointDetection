311a1f61b5cdb350f515ca27d64e9de2746762f0
Travis CI <travis@travis-ci.org>
2018-09-21 19:51:44 +0000

    Deploy code docs to GitHub Pages Travis build: 387
    Commit: 2ee63b058ed87dfee1958fce14e3b5d060865032

--- html/classop_1_1_video_capture_reader.js
+++ html/classop_1_1_video_capture_reader.js
@@ -7,7 +7,7 @@ var classop_1_1_video_capture_reader =
     [ "getNextFrameName", "classop_1_1_video_capture_reader.html#a06348fd9a290fc2ece2f3c2e4dc9bc70", null ],
     [ "getRawFrame", "classop_1_1_video_capture_reader.html#af57cc02f86ee0c29ccb83f1f7c37bec2", null ],
     [ "getRawFrames", "classop_1_1_video_capture_reader.html#a07abbb0001dd9972ab416eb619812043", null ],
-    [ "isOpened", "classop_1_1_video_capture_reader.html#a5d28ea704d629d983280ece0a530d732", null ],
+    [ "isOpened", "classop_1_1_video_capture_reader.html#ae863adc39055fc651d409cc350f26617", null ],
     [ "release", "classop_1_1_video_capture_reader.html#a2f73e10efe7f9b24a6cb75af2167de58", null ],
     [ "resetWebcam", "classop_1_1_video_capture_reader.html#ab85b68c93854dd7c2ad437477e819506", null ],
     [ "set", "classop_1_1_video_capture_reader.html#ab2929b7d2d002b58ebaf7b9b56999cca", null ]
